f667fe87f70653a026d68bdcb5de357fd3a38e9e
hikobend <hiko.bkend@gmail.com>
2022-12-14 22:12:45 +0900

    validate

--- main.go
+++ main.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
+	"github.com/go-playground/validator"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type JsonRequest struct {
-	FieldStr  string `json:"field_str"`
-	FieldInt  int    `json:"field_int" validate:"gte=0,lt=130"`
-	FieldBool bool   `json:"field_bool"`
+type User struct {
+	First string `json:"first" validate:"required"` //必須パラメータ
+	Last  string `json:"last" validate:"required"`  //必須パラメータ
 }
 
 func main() {
 	r := gin.Default()
-	validate := validator.New() //インスタンス生成
-	r.POST("/postjson", func(c *gin.Context) {
-		var json JsonRequest
-		errors := validate.Struct(json) //バリデーションを実行し、NGの場合、ここでエラーが返る。
-		if errors != nil {
-			log.Fatal(errors)
-		}
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"str": json.FieldStr, "int": json.FieldInt, "bool": json.FieldBool})
-	})
+	r.POST("/create", Insert)
 
 	r.Run()
+
+}
+
+func Insert(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:password@(localhost:3306)/local?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//バリデーション対象のデータをセット
+	var user User
+	validate := validator.New()  //インスタンス生成
+	err = validate.Struct(&user) //バリデーションを実行し、NGの場合、ここでエラーが返る。
+	if err != nil {
+		log.Fatal(err)
+	}
+	// c.ShouldBindJSON(&user)
+
+	insert, err := db.Prepare("INSERT INTO users(first, last) VALUES (?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	insert.Exec(user.First, user.Last)
 }
